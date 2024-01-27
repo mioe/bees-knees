68087f3cfa0e611b25f061be4867d053c5db7ca5
mioe <mioe.boss@gmail.com>
2024-01-27 18:12:09 +0300

    set createWebHistory route mode

--- src/modules/vue-router.ts
+++ src/modules/vue-router.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import routes from '~pages'
 import NotFound from '~/404.vue'
 
 const router = createRouter({
-	history: createWebHashHistory(),
+	history: createWebHistory(),
 	routes: [
 		...routes,
 		{ path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
